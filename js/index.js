f61c7955b03c7d80328af88edfab52ba3edabff4
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2023-11-29 16:19:40 -0300

    feat: add add-to-cart function to product modal

--- js/index.js
+++ js/index.js
@@ -78,7 +78,6 @@ class Producto {
         productoNombre.addEventListener('click', (e) => {
             let boton = e.target;
             traerProductoid(parseInt(boton.id))
-
         })
         productoNombre.innerHTML = this.nombre;
         let productoDescrip = d.createElement('p');
@@ -130,7 +129,6 @@ class Carrito {
      */
     agregar(objeto){
         this.producto.push(objeto);
-
     }
     /**
      * 
@@ -249,32 +247,32 @@ const traerProductosCategoria = async function (categoria) {
         }) )
         mostrarProductos (productos);
 }
+
 const traerProductoid = async function (id) {
     await fetch(`./../acciones/get-productos.php?id=${id}`)
         .then(algo => algo.json())
-        .then (resultado => agregarDatosModal(cargarProductos(resultado))  )
-        
+        .then (resultado => agregarDatosModal(cargarProductos(resultado)))
 }
 
 /**
  * Toma un elemento y le agrega el escuchhador del evento click para ejecutar la funcion que agrega los valores al mini carrito dentro del modal del producto
  */
-function agregarBotonModal() {
+function agregarBotonModal(producto) {
+    console.log(producto)
     let boton = d.getElementById('botonProducto').children[1];
+
     if (boton.id == 'cambiarIDBoton') {
         boton.innerHTML = `Agregar <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16"><path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/><path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>`;
         boton.addEventListener('click', (e) => {
-            let boton = e.target;
-            for (let i = 0; i < productos.length; i++) {
-                if (parseInt(boton.id) == productos[i].id) {
-                    carrito.agregar(productos[i])
-                    break
-                }
+            let btn = e.target
+            if (parseInt(btn.id) == producto.id) {
+                carrito.agregar(producto)
+                mostrarMiniCarrito();
+                boton.id = 'cambiarIDBoton'
             }
-            mostrarMiniCarrito();
         })
     }
-    boton.id = `${this.id}productoboton`;
+    boton.id = `${producto.id}productoboton`;
 }
 /**
  * Crea el modal del producto
@@ -295,7 +293,7 @@ function agregarDatosModal(producto) {
     d.getElementById('descripProducto').innerHTML = producto.descrip_larga;
     d.getElementById('precioProducto').innerHTML = producto.precio;
     d.getElementById('precioCuotas').innerHTML = (producto.precio / 6).toFixed(2);
-    // this.agregarBotonModal();
+    agregarBotonModal(producto);
 }
 
 // Se declara el objeto carrito de clase Carrito
