7801d6a7f96fb00782777bd13ee65a41e98fe610
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2023-11-28 23:08:25 -0300

    feat: add function to return json data

--- acciones/get-productos.php
+++ acciones/get-productos.php
@@ -2,15 +2,7 @@
 
 require_once('../Clases/Producto.php');
 
-$productos = (new Producto)->catalogoCompleto();
+$data = (new Producto)->catalogoCompleto();
 
-
-echo '<pre>';
-echo json_encode($productos);
-echo '</pre>';
-
-echo '<pre>';
-print_r($productos);
-echo '</pre>';
-
-
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($data);
