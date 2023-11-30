938c329432ef682d04b232a2324d08c65f07f188
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2023-11-29 23:39:14 -0300

    feat: add basic functionality catche

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,29 @@
+caches.keys().then(cache => {
+    console.log("cache", cache)
+})
+
+
+self.addEventListener("install", (e) => {
+    console.log("install");
+    caches.open("basicApp").then((cache) => {
+        cache.addAll([
+            '../',
+            '../views/',
+            '../img/hero/',
+            '../img/dojos/',
+            '../img/historia/',
+            '../img/icons/',
+            '../img/logo/',
+            '../img/logo/SVG/',
+            '../img/misc/',
+            '../estilos/',
+            'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css',
+            'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js'
+        ]);
+    });
+});
+
+
 self.addEventListener("fetch", (e) => {
     const url = e.request.url;
     console.log(url);
