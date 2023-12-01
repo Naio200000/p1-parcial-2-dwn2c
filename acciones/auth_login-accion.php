9787fc41d45f657a79d369ba5dbdf4a53eb5066a
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2023-11-30 22:43:14 -0300

    feat: add return msg from validation

--- acciones/auth_login-accion.php
+++ acciones/auth_login-accion.php
@@ -6,8 +6,13 @@ $datosPOST = $_POST;
 $usuario = (new Usuario)->log_in($datosPOST['username'], $datosPOST['password']);
 
 
-echo '<pre>';
-print_r($datosPOST);
-print_r($usuario);
-var_dump($usuario);
-echo '</pre>';
+if ($usuario) {
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(['login' => true, 'username' => $usuario]);
+} elseif ($usuario === false) {
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(['login' => false, 'mensaje' => 'La contraseña no es correcta']);
+} else {
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(['login' => null, 'mensaje' => 'El usuario no se encontro en nuestra Base de datos']);
+}
