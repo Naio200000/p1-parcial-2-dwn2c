80e1b32b73abf6b2c2700837d4a15b20a34b10c5
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2023-11-30 23:32:38 -0300

    feat: add POST fetch login consult

--- js/login.js
+++ js/login.js
@@ -1,1 +1,19 @@
 const loginForm = document.getElementById('loginForm')
+
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let data = new FormData(loginForm)
+
+    fetch('../acciones/auth_login-accion.php', {
+        method: 'POST',
+        body: data })
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+
+
+
+})
+
